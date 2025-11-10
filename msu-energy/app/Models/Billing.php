ccf76dff8238cd10268a07df84c5d4dfaa74fabc
Justine <95575741+Tatin1@users.noreply.github.com>
2025-11-11 00:24:16 +0800

    new

--- msu-energy/app/Models/Billing.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-class Billing extends Model {
-    protected $fillable=['building_id','last_month_kwh','this_month_kwh','total_bill'];
-    public function building(){ return $this->belongsTo(Building::class); }
-}
--- msu-energy/app/Models/Building.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-class Building extends Model {
-    protected $fillable = ['code','name','is_online'];
-    public function meters(){ return $this->hasMany(Meter::class); }
-    public function billing(){ return $this->hasOne(Billing::class); }
-}
